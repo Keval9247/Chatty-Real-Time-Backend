d0e99f07ec6fe67fe33d7fb5d5877e4fe7b79be4
Keval9247 <kevaljeetbadarakhiya@gmail.com>
2025-03-08 22:11:09 +0530

    Initial REST API setup with CORS fix2

--- middleware/socket.js
+++ middleware/socket.js
@@ -49,7 +49,7 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Socket.IO server running on port ${PORT}`);
 });
